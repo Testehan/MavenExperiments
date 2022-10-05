47034b27a32442692bdde19ebbcb8268f059576c
DanTestehan <tdan89@yahoo.com>
2022-10-05 13:05:40 +0300

    added a new profiles example

---
+++ profile2Example/src/test/java/com/testehan/maven/AppTest.java
@@ -0,0 +1,17 @@
+package com.testehan.maven;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    void randomTest() {
+        float square = 2 * 2;
+        float rectangle = 2 * 2;
+        assertEquals(square, rectangle);
+    }
+}
